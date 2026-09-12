b9338ae86b8c1032dc5ad1c4bc88dd2f5dbb9ee0
Ivan Glushenkov <glushman966@gmail.com>
2026-09-12 10:19:45 +0300

    изменение схем вывода информации UserResponse и добавление валидации полей UserUpdate

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict, model_validator
 
 
 class UserCreate(BaseModel):
@@ -6,14 +6,21 @@ class UserCreate(BaseModel):
     username: str = Field(min_length=3, max_length=20)
     email: str
     age: int = Field(ge=18, le=100)
+    phone: str
 
 
 class UserResponse(BaseModel):
     """Схема вывода пользователя. Отвечает за исходящие данные API."""
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     username: str
     email: str
     age: int
+    phone: str
+    country: str
+
+
 
 
 class UserUpdate(BaseModel):
@@ -21,3 +28,11 @@ class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=3, max_length=20,)
     email: str | None = None
     age: int | None = Field(default=None, ge=18, le=100,)
+
+    @model_validator(mode="after")
+    def reject_none_values(self):
+        for field in self.model_fields_set:
+            if getattr(self, field) is None:
+                raise ValueError(f"{field} не может быть null")
+
+        return self
