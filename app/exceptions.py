7dadade865299281d4e5d8a36c7c80201a116107
Ivan Glushenkov <glushman966@gmail.com>
2026-09-13 23:21:32 +0300

    добавлено кастомное исключение CannotDeleteSelfError

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,2 +1,6 @@
 class UserAlreadyExistsError(Exception):
     """Пользователь с таким email уже существует."""
+
+
+class CannotDeleteSelfError(Exception):
+    """Попытка администратора удалить самого себя."""
